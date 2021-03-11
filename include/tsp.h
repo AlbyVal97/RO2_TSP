3f0f9726f2e0317b44be7c46cb0ccaffc18c6c06
AlbyVal97 <albertovalente1@gmail.com>
2021-03-11 17:15:57 +0100

    Changed project structure

--- include/tsp.h
+++ include/tsp.h
@@ -24,44 +24,35 @@ typedef struct {
 	int nnodes;   
 	double *xcoord;
 	double *ycoord;
+	int integer_costs;                   // = 1 for integer costs (rounded distances), 0 otherwise
 
 	// parameters 
-	//int model_type; 
-	//int old_benders;
-	//int randomseed;
-	//int num_threads;
+	int model_type; 
 	double timelimit;						// overall time limit, in sec.s
 	char input_file[1000];		  			// input file
-	char node_file[1000];		  			// cplex node file
-	//int available_memory;
-	//int max_nodes; 							// max n. of branching nodes in the final run (-1 unlimited)
-	//double cutoff; 							// cutoff (upper bound) for master
-	//int integer_costs;
-
-	//global data
-	double	tstart;								
-	double zbest;							// best sol. available  
-	double tbest;							// time for the best sol. available  
-	double *best_sol;						// best sol. available    
-	double	best_lb;						// best lower bound available  
-	double *load_min;						// minimum load when leaving a node
-	double *load_max;						// maximum load when leaving a node
-	
-	// model;     
-	int xstart;
-	int qstart;
-	int bigqstart;  						
-	int sstart;
-	int bigsstart;
-	int ystart;
-	int fstart;
-	int zstart;
+							
+	double zbest;							// value of the best sol. available
 
 } instance;
 
 //inline
 inline int imax(int i1, int i2) { return ( i1 > i2 ) ? i1 : i2; } 
 inline double dmin(double d1, double d2) { return ( d1 < d2 ) ? d1 : d2; } 
-inline double dmax(double d1, double d2) { return ( d1 > d2 ) ? d1 : d2; } 
+inline double dmax(double d1, double d2) { return ( d1 > d2 ) ? d1 : d2; }
+
+// Prints in a standard way the provided type of error
+void print_error(const char* err);
+
+// Computes specifically the euclidean distance between nodes i and j
+double dist(int i, int j, instance* inst);
+
+// Optimizes the provided instance of the TSP
+int TSPopt(instance* inst);
+
+// Computes the position of edge [i,j] inside the Cplex tableau
+int xpos(int i, int j, instance* inst);
+
+// Builds the tableau (variables and constraints)
+void build_model(instance* inst, CPXENVptr env, CPXLPptr lp);
 
 #endif   /* TSP_H_ */ 
